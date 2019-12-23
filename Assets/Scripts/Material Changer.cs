6987b25307e7294db23e970f7973dadb2c8adb7d
ashutoshkale <kaleashutosh05@gmail.com>
2019-12-22 19:35:35 -0800

    First Commit To Dissolve Shader

---
+++ Assets/Scripts/Material Changer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+
+    public Material ChangingMaterial;
+    public Material DefaultMaterial;
+    public GameObject defaultObject;
+
+
+
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Clear the Object");
+        defaultObject.GetComponent<MeshRenderer>().material = ChangingMaterial;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/materialChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialChanger : MonoBehaviour
+{
+    public Material ChangingMaterial;
+    public Material DefaultMaterial;
+    public GameObject defaultObject;
+
+
+
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Clear the Object");
+        defaultObject.GetComponent<MeshRenderer>().material = ChangingMaterial;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
